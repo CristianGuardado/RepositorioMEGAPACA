eafe939945fdd322b93c510cb1804bc7ae3600a6
CristianGuardado <20240365@ricaldone.edu.sv>
2026-04-13 12:07:57 -0600

    Customer

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,14 @@ import productRoutes from "./src/routes/products.js";
 import branchesRoutes from "./src/routes/branches.js";
 import employeesController from './src/routes/employees.js';
 import customerRoutes from "./src/routes/customer.js";
-
+import registerCustomerRoutes from './src/routes/registerCustomer.js';
+import cookieParser from 'cookie-parser';
 
 
 //Creo una constante que guarde Express
 const app = express();
+
+app.use(cookieParser())
 
 app.use(express.json());
 
@@ -16,5 +19,6 @@ app.use("/api/branches", branchesRoutes);
 app.use("/api/employees", employeesController);
 app.use("/api/customers", customerRoutes );
 //app.use("api/registerCustomer")
+app.use("/api/registerCustomer", registerCustomerRoutes)
 
 export default app;
---
+++ backend/src/controllers/registerAdminController.js
@@ -0,0 +1,4 @@
+import nodemailer from "nodemailer"; //Enviar correo
+import crypto from "crypto";//Generar codigo aleatorio
+import jsonwebtoken from "jsonwebtoken"; //token
+import bcryptjs from "bcryptjs" //Encriptar
