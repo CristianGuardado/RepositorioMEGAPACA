07ce5bf6d2245b04d07b1634ba48927c34940ec3
CristianGuardado <20240365@ricaldone.edu.sv>
2026-04-14 15:41:14 -0600

    Login

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import employeesController from './src/routes/employees.js';
 import customerRoutes from "./src/routes/customer.js";
 import registerCustomerRoutes from './src/routes/registerCustomer.js';
 import cookieParser from 'cookie-parser';
+import loginCustomersController from './src/routes/login.js';
 
 
 //Creo una constante que guarde Express
@@ -18,7 +19,8 @@ app.use("/api/products", productRoutes);
 app.use("/api/branches", branchesRoutes);
 app.use("/api/employees", employeesController);
 app.use("/api/customers", customerRoutes );
-//app.use("api/registerCustomer")
-app.use("/api/registerCustomer", registerCustomerRoutes)
+//app.use("/api/registerCustomer")
+app.use("/api/registerCustomer", registerCustomerRoutes);
+app.use("/api/login", loginCustomersController)
 
 export default app;
